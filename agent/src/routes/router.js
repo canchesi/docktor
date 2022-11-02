54ead9bf60dba31bc4a30f7b27d437945b75c0b9
Claudio <claudio.anchesi@outlook.it>
2022-11-02 20:10:24 +0100

    First commit.

---
+++ agent/src/routes/router.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express();
+const config = require('../config/config')[process.env.NODE_ENV || 'development'];
+
+router.listen(3000);
+
+router.get('/', (req, res) => {
+
+    //const options = {
+    //    socketPath: '/run/docker.sock',
+    //    path: '/images/json',
+    //};
+
+    //const clientRequest = http.request(options, (response) => {
+    //    response.on('data', data => res.json(JSON.parse(data)));
+    //    response.on('error', data => console.error(data));
+    //});
+
+    //clientRequest.end();
+
+});
