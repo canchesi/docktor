7b2112121297a268506835710df518e49825a8f3
Claudio Anchesi <claudio.anchesi@outlook.it>
2023-01-24 19:09:06 +0100

    Containerizzazione agent.

---
+++ agent/src/config/config.js
@@ -0,0 +1,8 @@
+const config = {
+    port: process.env.PORT,
+    key_path: process.env.KEY_PATH,
+    cert_path: process.env.CERT_PATH,
+    passphrase_path: process.env.PASSPHRASE_PATH
+}
+
+module.exports = config;
--- agent/src/public/app.js
+++ agent/src/public/app.js
@@ -1,12 +1,22 @@
+require('dotenv').config();
+const config = require('../config/config');
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const app = express();
 const cors = require('cors');
 const request = require('../controllers/request');
 
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.listen(3100);
+var server = https.createServer({
+    key: fs.readFileSync(config.key_path),
+    cert: fs.readFileSync(config.cert_path),
+    passphrase: fs.readFileSync(config.passphrase_path).toString()
+}, app);
+server.listen(config.port);
+//app.listen(3100);
 
 app.use(request);
